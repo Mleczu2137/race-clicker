4a0d46ff7c8e1d5fbad9038b48347d2236c47268
Mleczu2137 <159015499+Mleczu2137@users.noreply.github.com>
2024-03-25 09:47:41 +0000

    123

--- src/UpgradeButton.tsx
+++ src/UpgradeButton.tsx
@@ -1,14 +1,20 @@
 export function UpgradeButton({
   name,
   image,
+  level = 1,
+  price = 1
 }: {
   name: string;
   image?: string;
+  level: number;
+  price: number;
 }) {
   return (
     <button className="button">
+      <p>{level}</p>
       <img height={96} width={96} src={image} />
       <p>{name}</p>
+      <p>CENA: {price}$</p>
     </button>
   );
 }
